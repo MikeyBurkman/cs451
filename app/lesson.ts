b9e9e3fd3832b24862405bd0201fd17beb572a33
MikeyBurkman <michael.burkman@gmail.com>
2016-10-03 16:33:02 -0400

    Completed lesson

--- app/lesson.ts
+++ app/lesson.ts
@@ -1,4 +1,19 @@
 
-import {Animal, getAnimals} from './data';
+import {Animal} from './data';
+import * as data from './data';
+import * as util from './util';
 
-const allAnimals = getAnimals();
+const allAnimals = data.getAnimals();
+
+function isDangerous(animal: Animal): boolean {
+    return (animal.isVenomous ||
+        animal.attackedTrainer.some(util.inPastSixMonths));
+}
+
+function formatName(animal: Animal): string {
+    return animal.name + ' the ' + animal.species +
+    '; venomous: ' + animal.isVenomous + '\n';
+}
+
+const dangerousAnimals = allAnimals.filter(isDangerous);
+console.log(dangerousAnimals.map(formatName).join(''));
